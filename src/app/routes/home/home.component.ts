416defde913f5581dc5f1c72eff0972f01ab796f
Ashley Perera <ashley.perera@penpersoftware.co.uk>
2019-03-17 20:27:40 +0000

    Added blog, footer component and home routes

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    images = [
+        './assets/img/NDF-example-1.png',
+        './assets/img/NDF-example-2.png',
+        './assets/img/bandman-overview.png',
+    ];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
 
 }
